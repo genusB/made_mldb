bfff16c75ae795a6908c9e70521093c64c62f851
Jana Kutsko <pasteyourtext@gmail.com>
2022-10-22 23:33:24 +0300

    make calcs more straightforward and change var to biased

--- hw1/mapper_var.py
+++ hw1/mapper_var.py
@@ -3,20 +3,22 @@ import csv
 
 
 def main():
+    x2_sum = 0
+    x_sum = 0
     chunk_cnt = 0
-    chunk_mean = 0
-    chunk_variance = 0
     PRICE_COL = 9
 
     for line in csv.reader(sys.stdin):
         try:
             price = float(line[PRICE_COL])
-            chunk_variance = chunk_cnt * ((price - chunk_mean) / (chunk_cnt + 1)) ** 2 + \
-                             (chunk_cnt * chunk_variance) / (chunk_cnt + 1)
-            chunk_mean = (chunk_mean * chunk_cnt + price) / (chunk_cnt + 1)
+            x2_sum += price ** 2
+            x_sum += price
             chunk_cnt += 1
         except ValueError:
             continue
+
+    chunk_mean = x_sum / float(chunk_cnt)
+    chunk_variance = (x2_sum - chunk_cnt * chunk_mean ** 2) / chunk_cnt
 
     print(chunk_cnt, chunk_mean, chunk_variance)
 
